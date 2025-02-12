a9c40387d9f54e005b10425822cc4165bfa02400
Tanner Flake <tannerflake@gmail.com>
2025-02-11 18:07:30 -0700

    (Kinda) fixed the login page styling issues.

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import AuthService from '../utils/auth';
+import '../index.css'; // Import the CSS file
 
 interface UserLoginInterface {
   username: string;
@@ -8,6 +9,7 @@ interface UserLoginInterface {
 
 const Login: React.FC = () => {
   const [userLogin, setUserLogin] = useState<UserLoginInterface>({ username: '', password: '' });
+
   const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setUserLogin({ ...userLogin, [name]: value });
@@ -38,10 +40,10 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className='login-container'>
+    <div className="login-container">
       <h2>Login</h2>
-      <form className='login-form' onSubmit={handleLogin}>
-        <div className='form-group'>
+      <form className="login-form" onSubmit={handleLogin}>
+        <div className="form-group">
           <label htmlFor="username">Username:</label>
           <input
             type="text"
@@ -49,10 +51,10 @@ const Login: React.FC = () => {
             name="username"
             value={userLogin.username}
             onChange={handleFormChange}
-            className='form-input'
+            className="form-input"
           />
         </div>
-        <div className='form-group'>
+        <div className="form-group">
           <label htmlFor="password">Password:</label>
           <input
             type="password"
@@ -60,10 +62,10 @@ const Login: React.FC = () => {
             name="password"
             value={userLogin.password}
             onChange={handleFormChange}
-            className='form-input'
+            className="form-input"
           />
         </div>
-        <button type="submit" className='btn btn-primary'>Login</button>
+        <button type="submit" className="btn btn-primary">Login</button>
       </form>
     </div>
   );
