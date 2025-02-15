66a7cc468779b855eb18ba6c44d03d01b44f3de9
Ace <matt.aceves@yahoo.com>
2025-02-15 14:11:37 -0700

    updated connection.ts

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -4,6 +4,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_DIALECT:', process.env.DB_DIALECT);
+
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(
