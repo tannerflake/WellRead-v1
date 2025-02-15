355d744454bd3d2b1ceb9f85357d155ba2d253d6
Ace <matt.aceves@yahoo.com>
2025-02-15 13:32:45 -0700

    updated server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, '../../client/dist'))); //added
 
 app.use(routes);
 
-app.get('*', (req, res) => {
+app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, '../../client/dist/index.html')); //added
 });
 
