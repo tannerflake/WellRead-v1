fb0789f4597d9e409e1eef9ddb4ac581decb5471
Ace <matt.aceves@yahoo.com>
2025-02-15 13:25:52 -0700

    updated server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import path from 'path'; //added
 import sequelize from './config/connection.js';
 import routes from './routes/index.js';
 
@@ -9,7 +10,14 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../../client/dist'))); //added
+
 app.use(routes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html')); //added
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
