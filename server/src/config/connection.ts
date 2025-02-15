68aabc74398029f19c4a114604f36d00db896090
Ace <matt.aceves@yahoo.com>
2025-02-15 14:26:54 -0700

    updated connection.ts

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -9,6 +9,7 @@ console.log('DB_USER:', process.env.DB_USER);
 console.log('DB_HOST:', process.env.DB_HOST);
 console.log('DB_PORT:', process.env.DB_PORT);
 console.log('DB_DIALECT:', process.env.DB_DIALECT);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
