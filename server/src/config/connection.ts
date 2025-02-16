dcd4fae536c54995521e040ae10f7bc467b475d2
Ace <matt.aceves@yahoo.com>
2025-02-15 18:57:45 -0700

    connect to postgresql

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -12,27 +12,24 @@ dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
 
 console.log('DB_NAME:', process.env.DB_NAME);
 console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_PORT:', process.env.DB_PORT);
-console.log('DB_DIALECT:', process.env.DB_DIALECT);
 console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_HOST:', process.env.DB_HOST);
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-      process.env.DB_NAME || '',
-      process.env.DB_USER || '',
-      process.env.DB_PASSWORD || '',
-      {
-        host: process.env.DB_HOST || 'localhost',
-        port: Number(process.env.DB_PORT) || 5432,
-        dialect: process.env.DB_DIALECT as any || 'postgres',
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-        logging: console.log, // Enable logging for Sequelize
-      }
-    );
+let sequelize: Sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME || '',
+    process.env.DB_USER || '',
+    process.env.DB_PASSWORD || '',
+    {
+      host: process.env.DB_HOST || 'localhost',
+      dialect: 'postgres',
+    },
+  );
+}
 
 sequelize.authenticate()
   .then(() => {
